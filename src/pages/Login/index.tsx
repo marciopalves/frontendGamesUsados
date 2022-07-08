9f5dc0c97dd9521c581d0c06de4f04e804dd6424
pedrofsn <pedrokra@gmail.com>
2022-07-08 00:06:13 -0300

    Sending login with node fetch (working!)

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,55 +1,83 @@
 import { useContext, useState } from "react"
 import { AuthContext } from "../../contexts/Auth/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { Formik, Field, Form, FormikHelpers } from "formik";
 import './Index.css';
 
 export const Login = () => {
     const auth = useContext(AuthContext);
     const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
 
-  const onSubmit = (form: React.FormEvent<HTMLFormElement>) => {
-        console.log('pegou');
-        console.log(email);
-        console.log(password);
-
-        const isLogged = auth.signin(email, password);
-        console.log("recebeu: " + isLogged);
-        form.preventDefault();
-      };
+    interface Values {
+      password: string;
+      email: string;
+    }
 
     return (
-        <div className="user-login">
-            <form onSubmit={onSubmit}>
-                <h2 className="user-login__title">Login Games Usados</h2>
+         <div>
+               <h1>Signup</h1>
+               <Formik
+                 initialValues={{
+                   password: '',
+                   email: '',
+                 }}
+                 onSubmit={(
+                   values: Values,
+                   { setSubmitting }: FormikHelpers<Values>
+                 ) => {
+                   setTimeout(() => {
+                        console.log(values.email);
 
-                <div className="user-login__form-control">
-                    <label htmlFor="email">E-mail</label>
-                    <input 
-                        id="email" 
-                        type="text" 
-                        value={email} 
-                        onChange={ e => setEmail(e.target.value)} 
-                        placeholder="Informe seu e-mail"
-                    />
-                </div>
+                        let body = {
+                            email: values.email,
+                            password: values.password
+                        };
 
-                <div className="user-login__form-control">
-                    <label htmlFor="password">Senha</label>    
-                    <input 
-                        id="password"
-                        type="password" 
-                        value={password} 
-                        onChange={ e => setPassword(e.target.value)} 
-                        placeholder="Informe sua senha"
-                    />
-                </div>                
-                
-                <button className="user-login__submit-button">
-                        Entrar 
-                </button>
-            </form>
-        </div>
+                        let stringBody = JSON.stringify(body);
+                        console.log(stringBody);
+
+                        let token = "";
+
+//                         fetch("http://localhost:8080/platforms").then(resp => resp.json())
+                        fetch("http://localhost:8080/auth", {
+                            method: 'POST',
+                            body: stringBody,
+                            headers: {
+                                "Authorization": `Bearer: ${token}`,
+                                'Content-Type': 'application/json',
+                                'Access-Control-Allow-Origin': '*'
+                            }
+                        }).then(resp => resp.json())
+                        .then(resp => console.log(resp));
+
+//                         auth.signin(values.email, values.password);
+//                      alert(JSON.stringify(values, null, 2));
+                     setSubmitting(false);
+                   }, 500);
+                 }}
+               >
+                 <Form>
+                 <h2 className="user-login__title">Login Games Usados</h2>
+                   <div className="user-login__form-control">
+                                       <label htmlFor="email">E-mail</label>
+                   <Field
+                     id="email"
+                     name="email"
+                     placeholder="seuemail@email.com"
+                     type="email"
+                   />
+                    </div>
+
+
+                    <div className="user-login__form-control">
+                                       <label htmlFor="password">Senha</label>
+                   <Field id="password" name="password" placeholder="John" />
+                    </div>
+
+                   <button className="user-login__submit-button"
+                   type="submit">Entrar</button>
+                 </Form>
+               </Formik>
+             </div>
     );
 }

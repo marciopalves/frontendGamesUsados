0ccd8199556e09b55486a3e3a1218cee924b6bfc
pedrofsn <pedrokra@gmail.com>
2022-07-08 00:26:17 -0300

    Returning data

--- src/contexts/Auth/AuthProvider.tsx
+++ src/contexts/Auth/AuthProvider.tsx
@@ -11,15 +11,7 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
 
     const signin = async ( email:string, password:string) => {
         const data = await api.signin(email, password)
-        console.log(data);        
-        //if(data.user) && data={.token}{
-        //if(data.usertype = 'ADMIN' && data.token){
-        if(data.token){  
-            //setUser(data.User);
-            //setLogin(token: data.token, usertype: data.usertype, type: data.usertype);
-            return true;
-        }
-        return false;
+        return data;
     }
 
     const signout = async () =>{
